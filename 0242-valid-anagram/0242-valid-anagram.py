359dc307d735736719a90f95f0ed6b3f8c85f932
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2025-09-29 19:37:43 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+        
+        
+        counter = {}
+
+        for i in s:
+            if i in counter:
+                counter[i] += 1
+            else:
+                counter[i] = 1
+
+
+        for j in t:
+            if j in counter:
+                counter[j] -= 1
+            else:
+                return False
+
+        return True
+
+    
+            
+
+
+
+        
