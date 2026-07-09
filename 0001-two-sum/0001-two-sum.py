0fba1b672c52ec1c1462c6c31bd6d613be6a6613
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-07-09 11:29:10 -0400

    Time: 1526 ms (41.05%), Space: 13.3 MB (16.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,19 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = dict()
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
 
+        if len(nums) == 2:
+            return [0,1]
         for i in range(len(nums)):
             complement = target - nums[i]
+            for j in range(i+1,len(nums)):
+                if nums[j] == complement:
+                    return [i,j]
 
-            for j in range(i+1, len(nums)):
-                if (complement == nums[j]):
-                    return[i,j]
-
-
-    
-            
+                
 
         
