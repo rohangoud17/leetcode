ee609167a4d28e93f6d12d3610e0c36b1b6fa4d5
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-07-09 11:21:51 -0400

    Time: 11 ms (95.85%), Space: 12.6 MB (50.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,21 +5,23 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if(len(s) != len(t)):
+        
+        if (len(s) != len(t)):
             return False
-        dic = {}
+        my_dict = {}
         for i in s:
-            if i in dic:
-                dic[i] += 1
+            if i in my_dict:
+                my_dict[i] += 1
             else:
-                dic[i] = 1
-
+                my_dict[i] = 1
         
         for j in t:
-            if j in dic and dic[j] > 0:
-                dic[j] -= 1
-            else:
+            if j in my_dict:
+                my_dict[j] -= 1
+        
+        for i in my_dict:
+            if (my_dict[i] != 0):
                 return False
-
-
+        
         return True
+
