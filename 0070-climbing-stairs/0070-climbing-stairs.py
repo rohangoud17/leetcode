5535cad7b5ed10b11eafe5873f399fd120965824
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-07-26 12:41:13 -0400

    Time: 0 ms (100%), Space: 12.3 MB (57.39%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if (n<= 2):
+            return n
+        
+        prev, curr = 1, 2
+
+        for i in range(3, n+ 1):
+            prev, curr = curr, curr+prev
+        
+        return curr
+        
