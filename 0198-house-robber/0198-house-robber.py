5b3a8160d304a24e988d64435d5960e8283391da
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-07-26 15:32:29 -0400

    Time: 0 ms (100%), Space: 12.2 MB (90.49%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        prev, curr = 0, 0
+        for num in nums:
+            prev, curr = curr, max(curr, prev + num)
+        
+        return curr
+
+        
