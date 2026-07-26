16e8e68455965fea9a3c456ecc41edb8e7a945b1
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-07-26 19:50:19 -0400

    Time: 3 ms (54.71%), Space: 13.3 MB (16.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,17 +7,17 @@ class Solution(object):
         """
 
         new_dict = {}
-        new_list = []
-
+        complement = 0
+        
         for i in range(len(nums)):
             complement = target - nums[i]
-            if complement in new_dict:
-                new_list = [new_dict[complement], i]
-            else:
-                new_dict[nums[i]] = i
-
-        return new_list
+            if (complement in new_dict):
+                return [i, new_dict[complement]]
+            new_dict[nums[i]] = i 
         
+        return -1
+        
+
         
 
  
