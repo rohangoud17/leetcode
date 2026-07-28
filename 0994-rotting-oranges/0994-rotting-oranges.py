3e4778179821a5a56d888339aa1ec7df1038da70
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-07-28 15:30:05 -0400

    Time: 5 ms (59.47%), Space: 12.5 MB (22.55%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -4,38 +4,39 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        if not grid:
-            return 0
-
-        q = collections.deque()
-        minutes = 0
-        fresh = 0
 
         ROWS, COLUMNS = len(grid), len(grid[0])
+        fresh = 0
+        q = collections.deque()
+        minutes = 0
+
 
         for r in range(ROWS):
             for c in range(COLUMNS):
-                if (grid[r][c] == 1):
+                if grid[r][c] == 1:
                     fresh += 1
-                elif (grid[r][c] == 2):
+                elif grid[r][c] == 2:
                     q.append([r,c])
-        
-        directions = [[1,0],[-1,0], [0,1], [0,-1]]
-        while q and fresh > 0:
+                
+        directions = [[1,0], [-1, 0], [0,1], [0,-1]]
+
+        while (q and fresh > 0):
+            
             for i in range(len(q)):
-                new_row, new_column = q.popleft()
+                new_row, new_col = q.popleft()
                 for dr, dc in directions:
-                    r, c = new_row + dr, new_column + dc
-                    
-                    if ((r < 0 or r >= ROWS) or (c < 0 or c >= COLUMNS) or grid[r][c] != 1):
+                    r, c = new_row + dr, new_col + dc
+                    if (r < 0 or r >= ROWS or c < 0 or c >= COLUMNS or grid[r][c] != 1 ):
                         continue
-                    
-                    grid[r][c] = 2
-                    q.append([r,c])
-                    fresh -= 1
+                    else:
+                        grid[r][c] = 2
+                        q.append([r,c])
+                        fresh -= 1
             minutes += 1
         
         return minutes if fresh == 0 else -1
+                    
+        
 
 
         
