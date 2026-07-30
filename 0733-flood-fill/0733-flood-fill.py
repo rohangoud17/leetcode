45a0cc0f84397e5da597d6a014d62abe0ae6a6d6
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-07-30 08:08:17 -0400

    Time: 0 ms (100%), Space: 12.4 MB (84.17%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -7,31 +7,35 @@ class Solution(object):
         :type color: int
         :rtype: List[List[int]]
         """
-        
 
-        ROWS, COLUMNS = len(image), len(image[0])
-        visit = set()
-
+        ROWS, COLS = len(image), len(image[0])
         q = collections.deque()
         og_color = image[sr][sc]
-        r, c = sr, sc
-        q.append((r, c))
-        visit.add((r,c))
         image[sr][sc] = color
+        r,c = sr,sc
+        
+        visit = set()
+
+        visit.add((r, c))
+
+        q.append((sr,sc))
 
         directions = [[1,0], [-1,0], [0,1], [0,-1]]
-
         while q:
-            new_row, new_col = q.popleft()
-            for dr, dc in directions:
-                r, c = new_row + dr, new_col + dc
-                if (r < ROWS and r >= 0 and c >= 0 and c < COLUMNS and image[r][c] == og_color and (r,c) not in visit):
+            new_r, new_c = q.popleft()
+            for dr,dc in directions:
+                r, c = new_r + dr, new_c + dc
+                if ( r >= 0 and r < ROWS and c >= 0 and c < COLS and image[r][c] == og_color and (r,c) not in visit):
                     image[r][c] = color
                     q.append((r,c))
                     visit.add((r,c))
         
         return image
 
+
+        
+
+        
 
             
 
