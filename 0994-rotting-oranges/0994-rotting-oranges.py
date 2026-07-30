cfb1bec6ef7f34314143bd5be00b2d6ba9584e8c
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-07-30 07:32:10 -0400

    Time: 0 ms (100%), Space: 12.4 MB (22.8%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,38 +5,43 @@ class Solution(object):
         :rtype: int
         """
 
-        ROWS, COLUMNS = len(grid), len(grid[0])
-        fresh = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
         q = collections.deque()
+
+        fresh = 0
         minutes = 0
 
-
         for r in range(ROWS):
-            for c in range(COLUMNS):
-                if grid[r][c] == 1:
-                    fresh += 1
-                elif grid[r][c] == 2:
+            for c in range(COLS):
+                if (grid[r][c] == 1):
+                    fresh+= 1
+                elif (grid[r][c] == 2):
                     q.append([r,c])
                 
-        directions = [[1,0], [-1, 0], [0,1], [0,-1]]
-
-        while (q and fresh > 0):
-            
+        
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        while (q and fresh >= 1):
             for i in range(len(q)):
-                new_row, new_col = q.popleft()
+                new_r, new_c = q.popleft()
                 for dr, dc in directions:
-                    r, c = new_row + dr, new_col + dc
-                    if (r < 0 or r >= ROWS or c < 0 or c >= COLUMNS or grid[r][c] != 1 ):
+                    r, c = new_r + dr, new_c + dc
+                    if (r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] != 1 or (r,c) in visit):
                         continue
-                    else:
-                        grid[r][c] = 2
-                        q.append([r,c])
-                        fresh -= 1
+                    grid[r][c] = 2
+                    visit.add((r,c))
+                    q.append([r,c])
+                    fresh -= 1
             minutes += 1
         
-        return minutes if fresh == 0 else -1
-                    
+        print(fresh)
         
+        return minutes if fresh == 0 else -1
+
+
+    
+
+
 
 
         
