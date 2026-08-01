a387b56cf602f4f1f7a1a6e631382a4e42324388
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-08-01 11:24:08 -0400

    Time: 11 ms (24.5%), Space: 12.6 MB (24.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -9,19 +9,25 @@ class Solution(object):
         cols = collections.defaultdict(set)
         squares = collections.defaultdict(set)
 
+        ROWS, COLS = len(board), len(board[0])
+    
         for r in range(9):
             for c in range(9):
 
-                if(board[r][c] == "."):
+                if (board[r][c] == '.'):
                     continue
                 
-                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3,c//3)]):
+                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r/3,c/3)] ):
                     return False
                 
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
-            
+                squares[(r/3,c/3)].add(board[r][c])
         
         return True
-        
+
+
+
+
+       
+             
