4d9ea821e1ed7f3a6f08f68c2a63bd8cd7f0b58b
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-08-01 13:15:05 -0400

    Time: 13 ms (39.05%), Space: 14 MB (97.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,20 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
 
-        my_dict = {}
-
-        for i in nums:
-            if i in my_dict:
-                my_dict[i] += 1
-            else:
-                my_dict[i] = 1
+        count = collections.Counter(nums)
         
-        result = []
 
-        for j in range(k):
-            highest_key = max(my_dict, key = my_dict.get)
-            result.append(highest_key)
-            my_dict.pop(highest_key)
+        return heapq.nlargest(k, count.keys(), key = count.get)
+
         
-        return result
+            
+
+
+        
+
         
