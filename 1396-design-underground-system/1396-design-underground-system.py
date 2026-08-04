c449931c540f54242c31d0e6fcd59ebe5dd7d918
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-08-04 17:47:02 -0400

    Time: 35 ms (20.29%), Space: 30.4 MB (25.43%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,53 +1,33 @@
-class UndergroundSystem(object):
+class UndergroundSystem:
 
     def __init__(self):
         self.startMap = {} #id -> (startStation, t)
-        self.TotalMap = {} #(startStation, endStation) -> (totalTime, count)
+        self.TotalMap = {} #(startStation, endStation) -> [totalTime, count]
         
 
-    def checkIn(self, id, stationName, t):
-        """
-        :type id: int
-        :type stationName: str
-        :type t: int
-        :rtype: None
-        """
-        self.startMap[id] = (stationName, t)
+    def checkIn(self, id: int, startStation: str, t: int) -> None:
+
+        self.startMap[id] = (startStation, t)
         
 
-    def checkOut(self, id, stationName, t):
-        """
-        :type id: int
-        :type stationName: str
-        :type t: int
-        :rtype: None
-        """
-
-        
+    def checkOut(self, id: int, endStation: str, t: int) -> None:
 
         startStation, time = self.startMap[id]
-        route = (startStation, stationName)
-
-        if route not in self.TotalMap:
-            self.TotalMap[route] = [0,0]
-            
-
-        self.TotalMap[route][0] += t-time
-        self.TotalMap[route][1] += 1
-        
-
-    def getAverageTime(self, startStation, endStation):
-        """
-        :type startStation: str
-        :type endStation: str
-        :rtype: float
-        """
 
         route = (startStation, endStation)
 
-        return float(self.TotalMap[route][0]) / float(self.TotalMap[route][1])
-
+        if route not in self.TotalMap:
+            self.TotalMap[route] = [0,0]
         
+        self.TotalMap[route][0] += t - time
+        self.TotalMap[route][1] += 1
+        
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+
+        route = (startStation, endStation)
+
+        return self.TotalMap[route][0]/self.TotalMap[route][1]
         
 
 
