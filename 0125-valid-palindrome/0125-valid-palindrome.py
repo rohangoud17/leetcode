39fc2a4b738e31832821c39356f068c62648041f
rohangoud17 <150305286+rohangoud17@users.noreply.github.com>
2026-09-02 16:57:28 -0400

    Time: 11 ms (31.49%), Space: 19.7 MB (53.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,20 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+
         res_string = ""
 
         for i in s:
-            if (i.isalnum()):
-                res_string += (i.lower())
+            if i.isalpha():
+                res_string += i.lower()
+
+            if i .isdigit():
+                res_string += i
         
-        rev_res = res_string[::-1]
-
-        return res_string == rev_res
-
+        return res_string == res_string[::-1]
+        
+        
+        
         
 
         
